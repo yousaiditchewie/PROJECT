2730622e9cd1586b9f26fa2c0269233828566900
Jacob Allen <jedijake@me.com>
2017-04-10 23:03:30 -0700

    add recipe model and output it to the DOM

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A Test', 'http://cdn-img.health.com/sites/default/files/styles/2000x2000/public/1489589880/easy-tandoori-chicken-yogurt-recipe.jpg?itok=3Wfmr-xg')
+  ];
   constructor() { }
 
   ngOnInit() {
